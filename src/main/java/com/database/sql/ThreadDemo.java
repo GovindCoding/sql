293e1a55a6577a8850cc7bfe981ff266854e9b36
Govind Ekshinge <govindeak@gmail.com>
2024-06-09 12:23:56 +0530

    Thread demo 09062024

---
+++ src/main/java/com/database/sql/ThreadDemo.java
@@ -0,0 +1,16 @@
+package com.database.sql;
+
+public class ThreadDemo extends Thread {
+
+    public void run() {
+        try {
+
+            System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+
+        } catch (Exception e) {
+
+            System.out.println("Exception is caught");
+
+        }
+    }
+}
